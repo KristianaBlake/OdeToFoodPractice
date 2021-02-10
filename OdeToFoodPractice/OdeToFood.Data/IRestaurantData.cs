c2854a4ed52df7257471bacc6eaa4b743d0803ba
Kristiana Blake <KristianaBlake@rightpoint.com>
2021-02-10 14:49:58 -0600

    registered a data service

--- OdeToFoodPractice/OdeToFood.Data/IRestaurantData.cs
+++ OdeToFoodPractice/OdeToFood.Data/IRestaurantData.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using OdeToFoodPractice.Core;
 
 namespace OdeToFood.Data
 {
     public interface IRestaurantData
     {
-        IEnumerable<Restaurant>
+        IEnumerable<Restaurant> GetAll();
     }
-}
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id= 1, Name="McFly's", Location="Chicago", Cuisine=CuisineType.None},
+                new Restaurant { Id=2, Name="Los Comanles", Location="Mexico", Cuisine=CuisineType.Mexian},
+                new Restaurant { Id=3, Name="Nepal House", Location="India", Cuisine=CuisineType.Indian},
+                new Restaurant { Id=4, Name="Bartoli's", Location="Italy", Cuisine=CuisineType.Italian}
+                
+            };
+        }
+ 
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r; 
+        }
+    }
--- OdeToFoodPractice/OdeToFoodPractice/Pages/Restaurants/List.cshtml.cs
+++ OdeToFoodPractice/OdeToFoodPractice/Pages/Restaurants/List.cshtml.cs
@@ -5,22 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
+using OdeToFoodPractice.Core;
 
 namespace OdeToFoodPractice.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
 
         public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
 
-        /*
-         ListModel is a Constructor
-         My Constructor can take a parameter of type IConfiguration*/
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, 
+            IRestaurantData restaurantData)
         {
             this.config = config;
+            this.restaurantData = restaurantData;
         }
         public void OnGet()
         {
