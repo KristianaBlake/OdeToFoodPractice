920d605b5821bd1ff92fd64babbf6b0f4c9bc4b5
Kristiana Blake <kristiana.blake@gmail.com>
2021-02-15 18:18:29 -0600

    added Create to the Data Access Service

--- OdeToFoodPractice/OdeToFood.Data/IRestaurantData.cs
+++ OdeToFoodPractice/OdeToFood.Data/IRestaurantData.cs
@@ -11,6 +11,7 @@ namespace OdeToFood.Data
         IEnumerable<Restaurant> GetRestaurantsByName(string Name);
         Restaurant GetById(int id);
         Restaurant Update(Restaurant updateRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
         int Commit();
     }
 
@@ -30,6 +31,13 @@ namespace OdeToFood.Data
         public Restaurant GetById(int id)
         {
             return restaurants.SingleOrDefault(r => r.Id == id); 
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
         }
 
         public Restaurant Update(Restaurant updatedRestaurant)
